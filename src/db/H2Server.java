86f0a6801326657b48278dfadde3b480ffc90346
Scott Faria <scott.faria@genedata.com>
2017-08-21 11:05:38 +0200

    Utility classes for database and updated libraries

---
+++ src/db/H2Server.java
@@ -0,0 +1,32 @@
+package db;
+
+import org.h2.tools.Server;
+
+import java.io.File;
+
+/**
+ * @author Scott Faria
+ */
+public final class H2Server {
+
+	// -------------------- Main --------------------
+
+	public static void main(String[] args) {
+		try {
+			Class.forName("org.h2.Driver");
+			try {
+				Server.shutdownTcpServer("tcp://localhost", "", true, true);
+			} catch (Throwable ignored) {} finally {
+				File dbFile = new File("db.h2.db");
+				if (dbFile.exists()) {
+					dbFile.delete();
+				}
+			}
+			Server.createTcpServer("-tcpAllowOthers").start();
+		} catch (Throwable th) {
+			th.printStackTrace(System.err);
+			System.exit(1);
+		}
+
+	}
+}
