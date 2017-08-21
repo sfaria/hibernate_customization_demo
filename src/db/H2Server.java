9f1c1507aa57834df160ced8407494964c417b17
Scott Faria <scott.faria@genedata.com>
2017-08-21 13:31:55 +0200

    database creation

--- src/db/H2Server.java
+++ src/db/H2Server.java
@@ -3,16 +3,38 @@ package db;
 import org.h2.tools.Server;
 
 import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * @author Scott Faria
  */
 public final class H2Server {
 
+	// -------------------- Private Statics --------------------
+
+	private static final String CONNECT_STRING = "jdbc:h2:tcp://localhost/./db";
+
 	// -------------------- Private Static Methods --------------------
 
-	private static void setupDBObjects() {
+	private static void setupDBObjects() throws SQLException {
+		try (Connection conn = createConnection(); Statement stmt = conn.createStatement()) {
+			//language=H2
+			stmt.execute("" +
+				"create table entity (\n" +
+				"  entity_id integer not null primary key,\n" +
+				"  entity_guid varchar2(100),\n" +
+				"  a_boolean varchar2(1),\n" +
+				"  last_update_date date not null,\n" +
+				"  creation_date date not null\n" +
+				")");
+		}
+	}
 
+	private static Connection createConnection() throws SQLException {
+		return DriverManager.getConnection(CONNECT_STRING, "test", "");
 	}
 
 	// -------------------- Main --------------------
@@ -29,6 +51,7 @@ public final class H2Server {
 				}
 			}
 			Server.createTcpServer("-tcpAllowOthers").start();
+			setupDBObjects();
 		} catch (Throwable th) {
 			th.printStackTrace(System.err);
 			System.exit(1);
